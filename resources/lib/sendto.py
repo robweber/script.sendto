d0ac6d4860987bceb5df6acddd373ee39cc3232e
Rob Weber <robweberjr@gmail.com>
2013-05-09 15:45:46 -0500

    missed this change when refactoring ConfusedTA's code

--- resources/lib/sendto.py
+++ resources/lib/sendto.py
@@ -25,7 +25,7 @@ class SendTo:
 
             if(selected_xbmc != -1):
                 #create a local host
-                local_host = XbmcHost("Local","127.0.0.1",utils.getSetting("local_host_port"),utils.getSetting("local_host_user"),utils.getSetting("local_host_password"))
+                local_host = XbmcHost("Local","127.0.0.1",8080,"","")
                 
                 #get the address of the host
                 remote_host = self.host_manager.getHost(selected_xbmc)
